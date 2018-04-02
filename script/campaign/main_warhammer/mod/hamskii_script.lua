b6a9616fbb6f2df68271f8ce873ee3591cf80c37
Tom Moreton <tom.moreton@live.co.uk>
2018-04-03 00:07:07 +0100

    Add log

--- script/campaign/main_warhammer/mod/hamskii_script.lua
+++ script/campaign/main_warhammer/mod/hamskii_script.lua
@@ -1,11 +1,12 @@
 function hamskii_script()
     if cm:is_new_game() then
         -- call your script functions here that you only want to run at the start of a campaign.
-        
+        output("==========HAMSKII IS ACTIVE==========");
+
         -- Force a certain stance of a faction towards another
         -- force_diplomacy("faction_key", "target_faction", "type listed below", offer, accept)
 
-        force_diplomacy_new("wh_main_emp_middenland", "wh_main_emp_empire", "vassal", true, true);
+        force_diplomacy("wh_main_emp_middenland", "wh_main_emp_empire", "vassal", true, true);
         -- TODO: Try force_diplomacy_new if force_diplomacy doesn't work
     else
         -- call functions here that you want to fire every time you load the game.
