f09c96dc4205a253dd837926f4814fedb956410f
Tom Moreton <tom.moreton@live.co.uk>
2018-04-02 23:46:36 +0100

    Initial commit

---
+++ script/campaign/main_warhammer/mod/hamskii_script.lua
@@ -0,0 +1,7 @@
+function hamskii_script()
+    if cm:is_new_game() then
+        -- call your script functions here that you only want to run at the start of a campaign.
+    else
+        -- call functions here that you want to fire every time you load the game.
+    end;
+end;
