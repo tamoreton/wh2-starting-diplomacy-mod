7c508d8bda2db547dd54c672c114c0c88f452b02
Tom Moreton <tom.moreton@live.co.uk>
2018-04-04 01:23:47 +0100

    Add dark elves

--- script/campaign/main_warhammer/mod/hamskii_script.lua
+++ script/campaign/main_warhammer/mod/hamskii_script.lua
@@ -3,6 +3,7 @@ function hamskii_script()
         -- call your script functions here that you only want to run at the start of a campaign.
         output("==========HAMSKII MOD IS ACTIVE==========");
         -- Empire Provinces
+        output("==========DEBUG: EMPIRE==========");
         cm:force_make_vassal("wh_main_emp_empire", "wh_main_emp_middenland");
         cm:force_make_vassal("wh_main_emp_empire", "wh_main_emp_averland");
         cm:force_make_vassal("wh_main_emp_empire", "wh_main_emp_talabecland");
@@ -14,6 +15,7 @@ function hamskii_script()
         cm:force_make_vassal("wh_main_emp_empire", "wh_main_emp_wissenland");
 
         -- Dwarf Realms
+        output("==========DEBUG: DWARFS==========");
         cm:force_make_vassal("wh_main_dwf_dwarfs", "wh_main_dwf_barak_varr");
         cm:force_make_vassal("wh_main_dwf_dwarfs", "wh_main_dwf_karak_azul");
         cm:force_make_vassal("wh_main_dwf_dwarfs", "wh_main_dwf_karak_hirn");
@@ -28,6 +30,7 @@ function hamskii_script()
         -- TODO: Greenskin Tribes
 
         -- Vampire Counts
+        output("==========DEBUG: VAMPIRE COUNTS==========");
         cm:force_declare_war("wh_main_vmp_schwartzhafen", "wh_main_vmp_rival_sylvanian_vamps"); -- Von Carstein
         cm:transfer_region_to_faction("wh_main_eastern_sylvania_eschen", "wh_main_vmp_vampire_counts");
         cm:transfer_region_to_faction("wh_main_eastern_sylvania_waldenhof", "wh_main_vmp_vampire_counts");
@@ -40,11 +43,13 @@ function hamskii_script()
         -- TODO: Beastmen
 
         -- Wood Elves
+        output("==========DEBUG: WOOD ELVES==========");
         cm:force_make_vassal("wh_dlc05_wef_wood_elves", "wh_dlc05_wef_argwylon");
         cm:force_make_vassal("wh_dlc05_wef_wood_elves", "wh_dlc05_wef_torgovann");
         cm:force_make_vassal("wh_dlc05_wef_wood_elves", "wh_dlc05_wef_wydrioth");
 
         -- Bretonnian Kingdoms
+        output("==========DEBUG: BRETONNIA==========");
         cm:force_make_vassal("wh_main_brt_bretonnia", "wh_main_brt_bordeleaux");
         cm:force_make_vassal("wh_main_brt_bretonnia", "wh_main_brt_carcassonne");
         cm:force_make_vassal("wh_main_brt_bretonnia", "wh_main_brt_artois");
@@ -62,7 +67,7 @@ function hamskii_script()
         -- TODO: Southern Realms
 
         -- High Elves
-        -- Not sure whether this should be wh2_main_hef_high_elves or wh2_main_hef_eataine
+        output("==========DEBUG: HIGH ELVES==========");
         cm:force_make_trade_agreement("wh2_main_hef_high_elves", "wh2_main_hef_avelorn");
         cm:force_make_trade_agreement("wh2_main_hef_high_elves", "wh2_main_hef_caledor");
         cm:force_make_trade_agreement("wh2_main_hef_high_elves", "wh2_main_hef_chrace");
@@ -74,8 +79,20 @@ function hamskii_script()
         cm:force_make_trade_agreement("wh2_main_hef_high_elves", "wh2_main_hef_yvresse");
         cm:force_make_vassal("wh2_main_hef_saphery", "wh2_main_hef_order_of_loremasters");  
 
-
-        -- TODO: Dark Elves
+        -- Dark Elves
+        output("==========DEBUG: DARK ELVES==========");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_cult_of_pleasure");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_bleak_holds");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_clar_karond");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_cult_of_excess");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_deadwood_sentinels");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_ghrond");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_hag_graef");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_har_ganeth");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_karond_kar");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_scourge_of_khaine");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wwh2_main_def_ssildra_tor");
+        cm:force_make_vassal("wh2_main_def_dark_elves", "wh2_main_def_the_forgebound");
 
         -- TODO: Lizardmen
 
