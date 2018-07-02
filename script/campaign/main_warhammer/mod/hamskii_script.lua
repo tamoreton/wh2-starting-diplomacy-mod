8ecccaaf11e4f433e33770f7b36a60301ed26257
Tom Moreton <tom.moreton@live.co.uk>
2018-07-02 09:07:27 +0100

    War declarations before call to force_make_vassal

--- script/campaign/main_warhammer/mod/hamskii_script.lua
+++ script/campaign/main_warhammer/mod/hamskii_script.lua
@@ -1,19 +1,13 @@
 -- Simulate vassalisation through diplomacy menu
-local function vassalise(master_faction_key, vassal_faction_keys)
-    out("vassalise() called, master_faction_key: " .. master_faction_key .. ", vassal faction_keys: " .. tostring(vassal_faction_keys));
+local human_factions = cm:get_human_factions();
+local player_1 = cm:get_faction(human_factions[1]);
+local player_2 = nil;
+-- only get player 2 if one exists
+if cm:is_multiplayer() then
+    player_2 = cm:get_faction(human_factions[2]);
+end;
 
-    local master_faction = cm:get_faction(master_faction_key);
-
-    local regions_to_make_visible = {};
-
-    local human_factions = cm:get_human_factions();
-    local player_1 = cm:get_faction(human_factions[1]);
-    local player_2 = nil;
-    -- only get player 2 if one exists
-    if cm:is_multiplayer() then
-        player_2 = cm:get_faction(human_factions[2]);
-    end;
-
+local function preserve_vassal_master_rules(master_faction_key, vassal_faction_keys)
 
     for h = 1, #vassal_faction_keys do
         if not is_string(vassal_faction_keys[h]) then
@@ -25,8 +19,6 @@ local function vassalise(master_faction_key, vassal_faction_keys)
         local vassal_faction = cm:get_faction(vassal_faction_key);
 
         if vassal_faction ~= player_1 or vassal_faction ~= player_2 then
-            table.insert(regions_to_make_visible, vassal_faction:region_list());
-
             -- force war between master faction and any of vassal faction enemies so vassal/master rules are preserved
             local vassal_enemies = {};
 
@@ -40,7 +32,7 @@ local function vassalise(master_faction_key, vassal_faction_keys)
                         local vassal_enemy_name = vassal_enemy:name();
 
                         out("Forcing war between [" .. master_faction_key .. "] and [" .. vassal_enemy_name .. "]");
-                        cm:force_declare_war(master_faction_key, vassal_enemy_name, false, false);
+                        cm:force_declare_war(master_faction_key, vassal_enemy_name, true, true);
 
                         if not vassal_enemy:is_ally_vassal_or_client_state_of(player_1) and not (player_2 and vassal_enemy:is_ally_vassal_or_client_state_of(player_2)) then
                             -- go through all vassalised factions as one vassal might not have the same enemies as another - all factions should be at war with the same set after this
@@ -51,7 +43,7 @@ local function vassalise(master_faction_key, vassal_faction_keys)
 
                                     if not other_vassal_faction:at_war_with(vassal_enemy) then
                                         out("Forcing war between [" .. other_vassal_faction_key .. "] and [" .. vassal_enemy_name .. "]");
-                                        cm:force_declare_war(other_vassal_faction_key, vassal_enemy_name, false, false);
+                                        cm:force_declare_war(other_vassal_faction_key, vassal_enemy_name, true, true);
                                     end;
                                 end;
                             end;
@@ -61,24 +53,34 @@ local function vassalise(master_faction_key, vassal_faction_keys)
             end;
         end;
     end;
+end;
+
+local function vassalise(master_faction_key, vassal_faction_keys)
+    out("vassalise() called, master_faction_key: " .. master_faction_key .. ", vassal faction_keys: " .. tostring(vassal_faction_keys));
+
+    local regions_to_make_visible = {};
 
     for i = 1, #vassal_faction_keys do
         local vassal_faction_key = vassal_faction_keys[i];
         local vassal_faction = cm:get_faction(vassal_faction_key);
 
-        if vassal_faction ~= player_1 or vassal_faction ~= player_2 then
+        table.insert(regions_to_make_visible, vassal_faction:region_list());
+
+        if vassal_faction == player_1 or vassal_faction == player_2 then
+            out("force_alliance called, faction_key_1: " .. master_faction_key .. ", faction_key_2: " .. vassal_faction_key);
+            cm:force_alliance(master_faction_key, vassal_faction_key);
+        else
             -- Force a faction to become the master of another faction
             out("force_make_vassal() called, master_faction_key: " .. master_faction_key .. ", vassal_faction_key: " .. vassal_faction_key);
             cm:force_make_vassal(master_faction_key, vassal_faction_key);
-        else
-            out("force_alliance called, faction_key_1: " .. master_faction_key .. ", faction_key_2: " .. vassal_faction_key);
-            cm:force_alliance(master_faction_key, vassal_faction_key);
         end;
     end;
 
     --[[
-    for j = 0, regions_to_make_visible:num_items() - 1 do
-        cm:make_region_seen_in_shroud(vassal_faction_keys[i], regions_to_make_visible:item_at(j):name());
+    for i = 1, #vassal_faction_keys do
+        for j = 0, regions_to_make_visible:num_items() - 1 do
+            cm:make_region_seen_in_shroud(vassal_faction_keys[i], regions_to_make_visible:item_at(j):name());
+        end;
     end;
     --]]
 end;
@@ -110,6 +112,92 @@ end;
 function hamskii_script()
     out("hamskii_script() called");
     if cm:is_new_game() then
+        -- Start by putting all the factions that need to be at war with each other, at war with each other
+        -- Do this before any calls to force_make_vassal to avoid running into a mess with factions asking allies to join
+
+        preserve_vassal_master_rules("wh_main_emp_empire", {
+            "wh_main_emp_middenland",
+            "wh_main_emp_averland",
+            "wh_main_emp_talabecland",
+            "wh_main_emp_ostland",
+            "wh_main_emp_nordland",
+            "wh_main_emp_hochland",
+            "wh_main_emp_ostermark",
+            "wh_main_emp_stirland",
+            "wh_main_emp_wissenland"
+        });
+
+        preserve_vassal_master_rules("wh_main_dwf_dwarfs", {
+            "wh_main_dwf_barak_varr",
+            "wh_main_dwf_karak_azul",
+            "wh_main_dwf_karak_hirn",
+            "wh_main_dwf_karak_izor",
+            "wh_main_dwf_karak_kadrin",
+            "wh_main_dwf_karak_norn",
+            "wh_main_dwf_karak_ziflin",
+            "wh_main_dwf_zhufbar",
+            "wh2_main_dwf_greybeards_prospectors"
+        });
+
+        cm:force_declare_war("wh_main_vmp_schwartzhafen", "wh_main_vmp_vampire_counts", false, false);
+
+        preserve_vassal_master_rules("wh_dlc05_wef_wood_elves", {
+            "wh_dlc05_wef_argwylon",
+            "wh_dlc05_wef_torgovann",
+            "wh_dlc05_wef_wydrioth"
+        });
+
+        preserve_vassal_master_rules("wh_main_brt_bretonnia", {
+            "wh_main_brt_bordeleaux",
+            "wh_main_brt_carcassonne",
+            "wh_main_brt_artois",
+            "wh_main_brt_bastonne",
+            "wh_main_brt_lyonesse",
+            "wh_main_brt_parravon",
+            "wh_main_vmp_mousillon",
+            "wh2_main_brt_knights_of_origo",
+            "wh2_main_brt_knights_of_the_flame",
+            "wh2_main_brt_thegans_crusaders"
+        });
+
+        cm:force_declare_war("wh_dlc08_nor_norsca", "wh_dlc08_nor_norsca", false, false);
+        preserve_vassal_master_rules("wh_dlc08_nor_norsca", {
+            "wh_dlc08_nor_helspire_tribe",
+            "wh_dlc08_nor_vanaheimlings"
+        });
+        preserve_vassal_master_rules("wh_dlc08_nor_wintertooth", {
+            "wh_dlc08_nor_goromadny_tribe",
+            "wh_dlc08_nor_naglfarlings"
+        });
+
+        preserve_vassal_master_rules("wh2_main_hef_saphery", { "wh2_main_hef_order_of_loremasters" });
+
+        preserve_vassal_master_rules("wh2_main_def_naggarond", {
+            "wh2_main_def_cult_of_pleasure",
+            "wh2_main_def_bleak_holds",
+            "wh2_main_def_clar_karond",
+            "wh2_main_def_cult_of_excess",
+            "wh2_main_def_deadwood_sentinels",
+            "wh2_main_def_ghrond",
+            "wh2_main_def_har_ganeth",
+            "wh2_main_def_karond_kar",
+            "wh2_main_def_scourge_of_khaine",
+            "wh2_main_def_ssildra_tor",
+            "wh2_main_def_the_forgebound"
+        });
+
+        preserve_vassal_master_rules("wh2_main_lzd_hexoatl", { "wh2_main_lzd_last_defenders" });
+
+        vassalise("wh2_dlc09_tmb_khemri",
+        {
+            "wh2_dlc09_tmb_lybaras",
+            "wh2_dlc09_tmb_exiles_of_nehek",
+            "wh2_dlc09_tmb_numas",
+            "wh2_dlc09_tmb_dune_kingdoms",
+            "wh2_dlc09_tmb_rakaph_dynasty",
+            "wh2_dlc09_tmb_followers_of_nagash"
+        });
+
         vassalise("wh_main_emp_empire", {
             "wh_main_emp_middenland",
             "wh_main_emp_averland",
@@ -157,7 +245,6 @@ function hamskii_script()
         cm:make_region_seen_in_shroud("wh_main_dwf_dwarfs", "wh_main_blightwater_karak_azgal");
         cm:make_region_seen_in_shroud("wh_main_dwf_dwarfs", "wh_main_southern_badlands_galbaraz");
 
-        cm:force_declare_war("wh_main_vmp_schwartzhafen", "wh_main_vmp_vampire_counts", false, false);
         cm:make_diplomacy_available("wh_main_vmp_schwartzhafen", "wh_main_vmp_mousillon");
         cm:transfer_region_to_faction("wh_main_eastern_sylvania_eschen", "wh_main_vmp_vampire_counts");
         cm:transfer_region_to_faction("wh_main_eastern_sylvania_waldenhof", "wh_main_vmp_vampire_counts");
@@ -182,7 +269,7 @@ function hamskii_script()
             "wh2_main_brt_knights_of_the_flame",
             "wh2_main_brt_thegans_crusaders"
         });
-        --[[
+
         vassalise("wh_dlc08_nor_norsca", {
             "wh_dlc08_nor_helspire_tribe",
             "wh_dlc08_nor_vanaheimlings"
@@ -193,7 +280,7 @@ function hamskii_script()
             "wh_dlc08_nor_naglfarlings"
         });
         cm:force_confederation("wh_dlc08_nor_wintertooth","wh_main_nor_varg");
-
+        
         create_alliance_network({
             "wh2_main_hef_eataine",
             "wh2_main_hef_caledor",
@@ -258,9 +345,8 @@ function hamskii_script()
             "wh2_dlc09_tmb_numas",
             "wh2_dlc09_tmb_dune_kingdoms",
             "wh2_dlc09_tmb_rakaph_dynasty",
-            --"wh2_dlc09_tmb_followers_of_nagash"
+            "wh2_dlc09_tmb_followers_of_nagash"
         });
-        --]]
     end;
     out("hamskii_script() complete");
 end;
